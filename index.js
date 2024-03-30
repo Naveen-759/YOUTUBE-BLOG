70b6d8dbaa58c91efedced1efd77ef28987dda3f
Naveen-759 <naveenku759@gmail.com>
2024-03-30 20:54:30 +0530

    committed

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const path = require("path");
 const express = require("express");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
+require("dotenv").config();
 
 const Blog = require("./models/blog");
 
@@ -14,9 +15,8 @@ const {
 
 const app = express();
 const PORT = 8000;
-
 mongoose
-  .connect("mongodb://localhost:27017/blogify")
+  .connect(process.env.MONGO_URI)
   .then((e) => console.log("MongoDB Connected"));
 
 app.set("view engine", "ejs");
