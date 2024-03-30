ed983de9ed91804cdedc47f2695ed97612e0ee7e
Naveen-759 <naveenku759@gmail.com>
2024-03-30 19:11:55 +0530

    completed

---
+++ middlewares/authentication.js
@@ -0,0 +1,20 @@
+function checkForAuthenticationCookie(cookieName) {
+  return (req, res, next) => {
+    const { validateToken } = require("../services/authentication");
+
+    const tokenCookieValue = req.cookies[cookieName];
+    if (!tokenCookieValue) {
+      return next();
+    }
+
+    try {
+      const userPayload = validateToken(tokenCookieValue);
+      req.user = userPayload;
+    } catch (error) {}
+    return next();
+  };
+}
+
+module.exports = {
+  checkForAuthenticationCookie,
+};
